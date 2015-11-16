c0ba6576e139c5cf2c6bc52831b25f58a8aaa6fc
Stevo5o <stevo.joc@gmail.com>
2015-11-16 13:05:49 +0000

    Expressions :skull:

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Handlebars-basic
+1. Templates
+2. Expressions
+3. Context
+4. Helpers
+5. Block helpers
+
+## Resources
+[Learn Handlebars in 10 Minutes or Less](http://tutorialzine.com/2015/01/learn-handlebars-in-10-minutes/ "Title")
--- javascripts/scripty.js
+++ javascripts/scripty.js
@@ -1,16 +1,18 @@
 $(function() {
 'use strict';
 // template script 
-var theTemplateScript = $("#address-template").html();
+var theTemplateScript = $("#expression-template").html();
 
 // compile the template
 var theTemplate = Handlebars.compile(theTemplateScript);
 
 // define data
 var context = {
-    "city": "Dublin",
-    "street": "O'Connor",
-    "number": "13λ"    
+    "description": {
+        "escaped": "Using {{}} brackets will result in escaped HTML:",
+        "unescaped": "Using {{{}}} will leave the context as it is:"
+    },
+    "example": "<button> Hello </button>"
 };
 
 // pass data to theTemplate
