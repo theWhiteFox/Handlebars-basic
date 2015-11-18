e58ff79befb43a571f87bed40888b1cfd2dbbbe3
Stevo5o <stevo.joc@gmail.com>
2015-11-18 22:17:56 +0000

    Changes :neckbeard:

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Handlebars-basic
+![](http://handlebarsjs.com/images/handlebars_logo.png)
+
 1. Templates
 2. Expressions
 3. Context
@@ -11,3 +13,9 @@
 [Built-In Helpers](http://handlebarsjs.com/builtin_helpers.html "Title")
 
 [Block Helpers](http://handlebarsjs.com/block_helpers.html "Title")
+
+- [Handlebars.js](http://handlebarsjs.com/ "Title) – Handlebars’ home is full of great documentation and examples
+- [Try Handlebars.js](http://tryhandlebarsjs.com/ "Title") – try out different Handlebars scenarios in your browser (it is for an older version of handlebars, unfortunately, but the basics are pretty much the same)
+- [Handlebars Helpers](https://github.com/assemble/handlebars-helpers "Title") – a collection of 130+ handlebars helpers in ~20 categories.
+- [SWAG](https://github.com/elving/swag) – more handlebars helpers
+- [Handlebars API Reference](http://handlebarsjs.com/reference.html)
--- javascripts/scripty.js
+++ javascripts/scripty.js
@@ -6,14 +6,14 @@ $(function () {
     
     // this is the block helper
     // the name of the helper is provided as the first parameter - in this case 'uppercase'
-    Handlebars.registerHelper('uppercase', function(options) {
+    Handlebars.registerHelper('uppercase', function (options) {
         
-    // "this" is the context that existed when calling the helper
+        // "this" is the context that existed when calling the helper
     
-    /* the options object has a special function - fn. This is a complied version of the template version of the template that is contained between the opeing and closing blocks of this helper. To get a string, call fn with the context: */
-    
-    return options.fn(this).toUpperCase();
-    
+        /* the options object has a special function - fn. This is a complied version of the template version of the template that is contained between the opeing and closing blocks of this helper. To get a string, call fn with the context: */
+
+        return options.fn(this).toUpperCase();
+
     });
     
     // compile the template
@@ -29,6 +29,30 @@ $(function () {
 
     // add the complied html to the div
     $('.content-placeholder').html(theCompiledHtml);
+
+    // names
+    var template = $('#myTemplate').html();
+
+    var data = {
+        "title": "A Song of Fire and Ice",
+        "names": [
+            { "name": "John" },
+            { "name": "Ygritte" }
+        ]
+    };
+
+    var compiledTemplate = Handlebars.compile(template);
+
+    var result = compiledTemplate(data);
+
+    $('.content-placeholder-two').html(result);
+
+    var template = '<h1>{{title}}</h1><ul>{{#names}}<li>{{name}}</li>{{/names}}</ul>';
+    var data = { "title": "Story", "names": [{ "name": "Tarzan" }, { "name": "Jane" }] };
+
+    var result = Mustache.render(template, data);
+    
+    document.body.innerHTML = result;
 
 });
 
