66621acfbb82a9651b2e20cd2090d2bf39cd4327
Stevo5o <stevo.joc@gmail.com>
2015-11-22 18:20:21 +0000

    Iteration :fist:

--- javascripts/scripty.js
+++ javascripts/scripty.js
@@ -1,78 +1,11 @@
 $(function () {
     'use strict';
-    
-    // // grab the template script
-    // var theTemplateScript = $("#block-expressions-template").html();
-    
-    // // this is the block helper
-    // // the name of the helper is provided as the first parameter - in this case 'uppercase'
-    // Handlebars.registerHelper('uppercase', function (options) {
-        
-    //     // "this" is the context that existed when calling the helper
-    
-    //     /* the options object has a special function - fn. This is a complied version of the template version of the template that is contained between the opeing and closing blocks of this helper. To get a string, call fn with the context: */
 
-    //     return options.fn(this).toUpperCase();
+    var theData = { customers: [{ firstName: "Rick", lastName: "Grimes", age: 39 }, { firstName: "Daryl", lastName: "Dixon", age: 36 }] };
 
-    // });
-    
-    // // compile the template
-    // var theTemplate = Handlebars.compile(theTemplateScript);
-    
-    // // define the data object
-    // var context = {
-    //     "code": "up up down down left right left right b a select start"
-    // };
-    
-    // // pass data to theTemplate
-    // var theCompiledHtml = theTemplate(context);
-
-    // // add the complied html to the div
-    // $('.content-placeholder').html(theCompiledHtml);
-
-    // // names
-    // var template = $('#myTemplate').html();
-
-    // var data = {
-    //     "title": "A Song of Fire and Ice",
-    //     "names": [
-    //         { "name": "John" },
-    //         { "name": "Ygritte" }
-    //     ]
-    // };
-
-    // var compiledTemplate = Handlebars.compile(template);
-
-    // var result = compiledTemplate(data);
-
-    // $('.content-placeholder-two').html(result);
-
-    var data = {
-        "title": "Languages",
-        languages: [
-            { name: "<strong>JavaScript</strong>", year: "1995", developer: { firstName: "Brendan", lastName: "Eich" } },
-            { name: "<strong>Java</strong>", year: "1995", developer: { firstName: "James", lastName: "Gosling" } },
-            { name: "<strong>C#</strong>", year: "2000" },
-            { name: "<strong>PHP</strong>", year: "1998", developer: { firstName: "Rasmus", lastName: "Lerdorf" } }
-        ],
-        type: "Programming languages"
-    }
-
-    Handlebars.registerHelper("formatLanguage", function (language) {
-        return '-<code>' + language + '-</code>';
-    });
-
-    Handlebars.registerHelper("list", function (items, options) {
-        var out = "<ul>";
-
-        for (var index = 0, l = items.length; index < l; index++) {
-            out = out + "<li>" + options.fn(items[index]) + "</li>";
-        }
-        return out + "</ul>"
-    });
-  
-    var html = Handlebars.templates['languages.tpl'];
-    $("#languages").html(html(data));
+    var theTemplateScript = $('#header-tpl').html();
+    var theTemplate = Handlebars.compile(theTemplateScript);
+    $('#customers').append(theTemplate(theData));
 
 });
 
